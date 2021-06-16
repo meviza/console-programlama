1be8111ed2ff3edfec2547b7dc9ff195887f37f1
Kerem Çelik <kerem_newton@hotmail.com>
2021-06-16 21:18:35 +0300

    C# 101 console-programlama

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz:");
+            String surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba"+ " " + name + " " + surname);
+        }
+    }
+}
